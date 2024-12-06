e61eacf90295d08b2de92dcdb4253f4a9514b438
Deyvis-1281 <foxandres1281@gmail.com>
2024-12-06 17:29:48 -0500

    Perdi tiempo con lo del git, no encontraba la carpeta por el afan

--- src/PuntoNueve/PuntoNueve.java
+++ src/PuntoNueve/PuntoNueve.java
@@ -1,9 +1,21 @@
 package PuntoNueve;
+import java.util.ArrayList;
+import java.util.List;
 
 class Main {
     public static void main(String[] args) {
 
-
-
+        String[] datos = {"antony", "32", "fredy", "45", "maria", "23"};
+        List<String> nombres = new ArrayList<>();
+        List<String> edades = new ArrayList<>();
+        for (String dato : datos) {
+            if (dato.matches("\\d+")) {
+                edades.add(dato);
+            } else {
+                nombres.add(dato);
+            }
+        }
+        System.out.println("Nombres: " + nombres);
+        System.out.println("Edades: " + edades);
     }
 }
--- src/PuntoOcho/PuntoOcho.java
+++ src/PuntoOcho/PuntoOcho.java
@@ -6,7 +6,7 @@ class Main {
     public static void main(String[] args) {
 
     String linea = "antony|fredy|julian|marcos";
-        List<String> lineaSeparada = new ArrayList<>(Arrays.asList(linea.split("\\|")));
+        List<String> lineaSeparada = new Array<>(Arrays.asList(linea.split("\\|")));
 
     for(String lista : lineaSeparada){
         System.out.print(lineaSeparada);
